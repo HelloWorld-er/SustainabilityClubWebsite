5a78497b0a5d0fdf6400bda3abf9671c77e0c2e3
Yimin Liu <122608577+HelloWorld-er@users.noreply.github.com>
2026-03-24 01:12:31 +0800

    Delete sustainability-club-website/src/lib directory

--- sustainability-club-website/src/lib/site.js
+++
@@ -1,9 +0,0 @@
-const DEFAULT_SITE_URL = 'http://localhost:3000';
-
-export const SITE_NAME = 'Sustainability Club';
-
-export const getSiteUrl = () => {
-  const value = process.env.NEXT_PUBLIC_SITE_URL || process.env.SITE_URL || DEFAULT_SITE_URL;
-  return value.replace(/\/$/, '');
-};
-
